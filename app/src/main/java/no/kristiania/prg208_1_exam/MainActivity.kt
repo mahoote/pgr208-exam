193e667efb949070ce9cdd5be43bc2d830ca0b95
Martin Teigen <martin@teigen.biz>
2022-03-31 14:34:22 +0200

    HeaderFragment.kt and MainActivity.kt setup

--- app/src/main/java/no/kristiania/prg208_1_exam/MainActivity.kt
+++ app/src/main/java/no/kristiania/prg208_1_exam/MainActivity.kt
@@ -2,10 +2,17 @@ package no.kristiania.prg208_1_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import no.kristiania.prg208_1_exam.fragments.HeaderFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val headerFragment = HeaderFragment()
+
+        // Add header fragment to activity
+        fragmentManager.beginTransaction().add(R.id.header_fragment_container, headerFragment).commit()
     }
 }
