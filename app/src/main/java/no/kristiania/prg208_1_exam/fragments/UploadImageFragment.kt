9e5e240c5bace8504b058f26a4ccc6e280c9af5a
Martin Teigen <martin@teigen.biz>
2022-04-04 13:54:52 +0200

    Details

--- app/src/main/java/no/kristiania/prg208_1_exam/fragments/UploadImageFragment.kt
+++ app/src/main/java/no/kristiania/prg208_1_exam/fragments/UploadImageFragment.kt
@@ -1,11 +1,16 @@
 package no.kristiania.prg208_1_exam.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import no.kristiania.prg208_1_exam.Globals
 import no.kristiania.prg208_1_exam.R
+import no.kristiania.prg208_1_exam.SearchActivity
 
 class UploadImageFragment : Fragment() {
 
@@ -14,6 +19,23 @@ class UploadImageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_image, container, false)
+        val v =  inflater.inflate(R.layout.fragment_upload_image, container, false)
+
+        v.findViewById<AppCompatButton>(R.id.uf_upload_search_btn).setOnClickListener{
+            startEmptyActivity(SearchActivity())
+        }
+
+        return v
+    }
+
+    // Dummy method!!
+    private fun startEmptyActivity(activity: Activity){
+        val activityClassName = activity::class.java.name
+        val currentActivity = Globals.getCurrentActivity(context)
+
+        if(!currentActivity.equals(activityClassName)) {
+            val intent = Intent(this.requireContext(), activity::class.java)
+            startActivity(intent)
+        }
     }
 }
