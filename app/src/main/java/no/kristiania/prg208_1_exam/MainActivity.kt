90621ade4384f757444685dc4c3dad8f02384c30
Martin Teigen <martin@teigen.biz>
2022-04-04 12:31:39 +0200

    When upload image fragment is visible and press back, remove fragment. Else if on main activity and press back, close app

--- app/src/main/java/no/kristiania/prg208_1_exam/MainActivity.kt
+++ app/src/main/java/no/kristiania/prg208_1_exam/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.os.HandlerCompat.postDelayed
 import androidx.fragment.app.Fragment
@@ -12,11 +13,16 @@ import no.kristiania.prg208_1_exam.fragments.HeaderFragment
 import no.kristiania.prg208_1_exam.fragments.UploadImageFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Globals.setHeaderFragment(supportFragmentManager)
+        fragmentManager = supportFragmentManager
+
+        Globals.setHeaderFragment(fragmentManager)
 
         val selectImage = findViewById<AppCompatButton>(R.id.m_select_image_btn)
         val uploadImageFragment = UploadImageFragment()
@@ -27,9 +33,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun switchFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-
         fragmentManager.beginTransaction()
             .replace(R.id.m_content_fragment_container, fragment, "content_fragment").commit()
+    }
+
+    override fun onBackPressed() {
+        val contentFragment = fragmentManager.findFragmentByTag("content_fragment")
+        Toast.makeText(applicationContext, "Back pressed", Toast.LENGTH_SHORT).show()
+
+        if (contentFragment != null) {
+            fragmentManager.beginTransaction().remove(contentFragment).commit()
+        } else {
+            finishAffinity()
+        }
     }
 }
--- app/src/main/java/no/kristiania/prg208_1_exam/SearchActivity.kt
+++ app/src/main/java/no/kristiania/prg208_1_exam/SearchActivity.kt
@@ -3,6 +3,7 @@ package no.kristiania.prg208_1_exam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Toast
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
