ace076a4318c3c8cdb36ae66c71b367ae12ec04d
Martin Teigen <martin@teigen.biz>
2022-04-04 14:36:20 +0200

    Add ImageAdapter.kt to print out recycler view. Add scroll view to SearchActivity.kt

--- app/src/main/java/no/kristiania/prg208_1_exam/SearchActivity.kt
+++ app/src/main/java/no/kristiania/prg208_1_exam/SearchActivity.kt
@@ -2,16 +2,46 @@ package no.kristiania.prg208_1_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import no.kristiania.prg208_1_exam.adapters.ImageAdapter
+import no.kristiania.prg208_1_exam.models.ResultImage
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    private val results: ArrayList<ResultImage> = arrayListOf(
+        ResultImage(R.mipmap.ic_launcher, "Test"),
+        ResultImage(R.mipmap.ic_launcher, "Test"),
+        ResultImage(R.mipmap.ic_launcher, "Test"),
+        ResultImage(R.mipmap.ic_launcher, "Test"),
+        ResultImage(R.mipmap.ic_launcher, "Test"),
+        ResultImage(R.mipmap.ic_launcher, "Test"),
+        ResultImage(R.mipmap.ic_launcher, "Test"),
+        ResultImage(R.mipmap.ic_launcher, "Test")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         Globals.setHeaderFragment(supportFragmentManager)
         overridePendingTransition(0, 0)
+
+        val adapter = ImageAdapter(results)
+        recyclerView = findViewById(R.id.s_results_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        adapter.setOnItemClickListener(object : ImageAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                Toast.makeText(applicationContext, "Button $position pressed", Toast.LENGTH_SHORT).show()
+            }
+
+        })
 
     }
 }
---
+++ app/src/main/java/no/kristiania/prg208_1_exam/models/ResultImage.kt
@@ -0,0 +1,6 @@
+package no.kristiania.prg208_1_exam.models
+
+data class ResultImage(
+    var img: Int,
+    var name: String
+)
