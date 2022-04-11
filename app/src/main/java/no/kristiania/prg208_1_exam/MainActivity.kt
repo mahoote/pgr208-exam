f6ac3eef316f4874d81b572255c6dd287560c1a1
Martin Teigen <martin@teigen.biz>
2022-04-11 12:54:19 +0200

    Add runtime permission

--- app/src/main/java/no/kristiania/prg208_1_exam/MainActivity.kt
+++ app/src/main/java/no/kristiania/prg208_1_exam/MainActivity.kt
@@ -1,21 +1,27 @@
 package no.kristiania.prg208_1_exam
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import android.widget.Toast
+import android.provider.Settings
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.os.HandlerCompat.postDelayed
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import no.kristiania.prg208_1_exam.fragments.HeaderFragment
 import no.kristiania.prg208_1_exam.fragments.UploadImageFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private val REQUEST_CODE = 200
     private lateinit var fragmentManager: FragmentManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +36,10 @@ class MainActivity : AppCompatActivity() {
         val uploadImageFragment = UploadImageFragment()
 
         selectImage.setOnClickListener {
-            switchFragment(uploadImageFragment)
+            if (askForPermissions()) {
+                // Permissions are already granted, do your stuff
+            }
+//            switchFragment(uploadImageFragment)
         }
     }
 
@@ -47,6 +56,69 @@ class MainActivity : AppCompatActivity() {
         } else {
             finishAffinity()
         }
+    }
+
+    private fun isPermissionsAllowed(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun askForPermissions(): Boolean {
+        if (!isPermissionsAllowed()) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this as Activity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                showPermissionDeniedDialog()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this as Activity,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_CODE
+                )
+            }
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission is granted, you can perform your operation here
+                } else {
+                    // permission is denied, you can ask for permission again, if you want
+                    //  askForPermissions()
+                }
+                return
+            }
+        }
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Permission Denied")
+            .setMessage("Permission is denied, Please allow permissions from App Settings.")
+            .setPositiveButton("App Settings",
+                DialogInterface.OnClickListener { dialogInterface, i ->
+                    // send to app settings if permission is denied permanently
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                })
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 
 
