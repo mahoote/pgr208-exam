52e8826a99c1fd2c0f8193159a41732b6d7a3452
Jonasthj <jonasthenderupjensen@gmail.com>
2022-04-20 15:04:52 +0200

    Data class for ResultImage.kt -> defaults to null if value not present. Get request now is successful

--- app/src/main/java/no/kristiania/prg208_1_exam/models/ResultImage.kt
+++ app/src/main/java/no/kristiania/prg208_1_exam/models/ResultImage.kt
@@ -1,13 +1,13 @@
 package no.kristiania.prg208_1_exam.models
 
 data class ResultImage(
-    val current_date: String,
-    val description: String,
-    val domain: String,
-    val identifier: String,
-    val image_link: String,
-    val name: String,
-    val store_link: String,
-    val thumbnail_link: String,
-    val tracking_id: String
+    val current_date: String? = null,
+    val description: String? = null,
+    val domain: String? = null,
+    val identifier: String? = null,
+    val image_link: String? = null,
+    val name: String? = null,
+    val store_link: String? = null,
+    val thumbnail_link: String? = null,
+    val tracking_id: String? = null
 )
